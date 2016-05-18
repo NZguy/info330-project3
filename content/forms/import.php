0239e826b146da21ad39dda55305c5cb0cf058b9
Kodlee Yin <kodlee@kodleeshare.net>
2016-05-17 19:18:29 -0700

    Completed import page

--- content/forms/import.php
+++ content/forms/import.php
@@ -5,9 +5,36 @@ use i330p3\template\StaticPage;
 $body = <<<HTML
 <div class="k-page-title k-container">
 	<div class="k-title">
-		Import Profiles
+		Importing Profiles
 	</div>
 </div>
+<div class="k-container">
+	<h2 class="k-title">Social Media</h2>
+	<div class="k-block-text">
+		We use social media to take a small peek at how you interact with friends and family, and
+		search for vehicles that would best suit your social and personal needs.
+	</div>
+	<div class="k-button k-fullscreen"><i class="fa fa-facebook"></i>Facebook</div>
+	<div class="k-button k-fullscreen"><i class="fa fa-google-plus"></i>Google Plus</div>
+	<div class="k-button k-fullscreen"><i class="fa fa-twitter"></i>Twitter</div>
+	<div class="k-button k-fullscreen"><i class="fa fa-instagram"></i>Instagram</div>
+	
+	<div class="k-spacer k-normal"></div>
+	<h2 class="k-title">Platforms</h2>
+	<div class="k-block-text">
+		These various other sources allow us to get a feel for how you'd like the car the handle.
+		This proprietary import algorithm selects your best interests, and lets us search for your
+		most desired vehicle.
+	</div>
+	<div class="k-button k-fullscreen"><i class="fa fa-steam"></i>Steam</div>
+	<div class="k-button k-fullscreen"><i class="fa fa-android"></i>Android</div>
+	<div class="k-button k-fullscreen"><i class="fa fa-deviantart"></i>DeviantArt</div>
+
+
+	<div class="k-spacer k-normal"></div>
+	<a href="/forms/personality" class="k-button k-fullscreen k-secondary">Continue to Personality Survey</a>
+</div>
+
 HTML;
 
 StaticPage::createContent()

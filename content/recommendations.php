40a305cac28a7d64447d0d444a8dc01130f246e7
Duncan Andrew <duncanandrew@live.com>
2016-05-19 14:10:17 -0700

    Various fixes

--- content/recommendations.php
+++ content/recommendations.php
@@ -11,11 +11,7 @@ Session::set(SessionKVs::TUTORIAL_KEY, SessionKVs::TUTORIAL_VALUE_ACTIVE);
 if(!Session::exists(SessionKVs::CAR_BOOKMARK_ARRAY)){
     $bookmarkArray = array();
     for($i = 0; $i < sizeof(Car::getCars()); $i++){
-        if($i <= 1){
-            $bookmarkArray[$i] = true;
-        }else{
-            $bookmarkArray[$i] = false;
-        }
+        $bookmarkArray[$i] = false;
     }
     $bookmarks = Session::set(SessionKVs::CAR_BOOKMARK_ARRAY, $bookmarkArray);
 }
@@ -24,43 +20,44 @@ if(!Session::exists(SessionKVs::CAR_BOOKMARK_ARRAY)){
 $carHtml = "";
 for ($i = 0; $i < sizeof(Car::getCars()); $i++) {
     $currentCar = Car::getCars()[$i];
-    $carHtml .= ' 
-<a href="/recommendations/detail?car=' .$i. '" class="d-car">
-	<div class="d-car-image">
-		<img src="' .$currentCar->image. '" alt="car"/>
+    $carHtml .= "<a href=\"/recommendations/detail?car={$currentCar->id}\" class=\"d-car\">
+	<div class=\"d-car-image\">
+		<img src=\"{$currentCar->image}\" alt=\"car\"/>
 	</div>
-	<div class="d-car-text">
-		<h2 class="d-title">' .$currentCar->year.' '.$currentCar->company.' '.$currentCar->model. '</h2>
-		<div class="d-desc">$' .number_format($currentCar->price). '</div>
+	<div class=\"d-car-text\">
+		<h2 class=\"d-title\">" .$currentCar->year." ".$currentCar->company." ".$currentCar->model. "</h2>
+		<div class=\"d-desc\">$" .number_format($currentCar->price). "</div>
 	</div>
 	<label>
-	    <input type="checkbox" class="d-car-bookmark-button"/> 
-        <div class="d-car-icon d-car-bookmark-empty"><i class="fa fa-star-o fa-lg"></i></div>
-        <div class="d-car-icon d-car-bookmark-full"><i class="fa fa-star fa-lg"></i></div>
+	    <input type=\"checkbox\" class=\"d-car-bookmark-button\"/> 
+        <div class=\"d-car-icon d-car-bookmark-empty\"><i class=\"fa fa-star-o fa-lg\"></i></div>
+        <div class=\"d-car-icon d-car-bookmark-full\"><i class=\"fa fa-star fa-lg\"></i></div>
 	</label>
+	<div class='k-clearfix'></div>
 </a>
-';
+";
 }
 
 $body = <<<HTML
-<div class="k-spacer k-normal"></div>
+<div class="d-rec-container">
+    <div class="k-spacer k-normal"></div>
 
-<div class='k-container'>
-    <h2 class='k-title'>Here's What We Found For You</h2>
-    <div class='k-block-text'>
-        We've sent our highly trained staff to find cars that fit your each and ever need and
-        here are the results. Star (bookmark) each vehicle that looks nice and to compare with our
-        patent pending car-parer-er in the Bookmarks tab.
+    <div class='k-container'>
+        <h2 class='k-title'>Here's What We Found For You</h2>
+        <div class='k-block-text'>
+            We've sent our highly trained staff to find cars that fit your each and ever need and
+            here are the results. Star (bookmark) each vehicle that looks nice and to compare with our
+            patent pending car-parer-er in the Bookmarks tab.
+        </div>
+    </div>
+    
+    <div class="k-spacer k-normal"></div>
+    $carHtml
+    <div class="k-spacer k-normal"></div>
+    <div class="k-container">
+        <a class="k-button k-fullscreen k-secondary" href="/bookmarks">Compare Bookmarked</a>
     </div>
 </div>
-
-<div class="k-spacer k-normal"></div>
-$carHtml
-<div class="k-spacer k-normal"></div>
-<div class="k-container">
-    <a class="k-button k-fullscreen k-secondary" href="/bookmarks">Compare Bookmarked</a>
-</div>
-
 
 <script src="/js/bookmarks.js" type="text/javascript"></script>
 HTML;

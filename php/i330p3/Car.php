fbeae0f48039251a1cb2982ffb240c029eaf51b5
Duncan Andrew <duncanandrew@live.com>
2016-05-18 22:42:05 -0700

    Added a ton more cars

--- php/i330p3/Car.php
+++ php/i330p3/Car.php
@@ -14,7 +14,13 @@ class Car {
 					1 => new Car(1, "Subaru", "WRX STI", 2016, 36998, "6K", "Beaverton (OR)", 82, 17, "Black", "Gray", "Manual 6 Speed", "4WD", "2.5L", 4, 305, 290, "https://img2.carmax.com/img/vehicles/13007523/1/v-0x8d3657f3e62559b/800.jpg"),
 					2 => new Car(2, "Ford", "F150 FX4", 2014, 39998, "20K", "Clackamas (OR)", 2, 15, "Black", "White", "Automatic", "4WD", "3.5L", 6, 365, 420, "https://img2.carmax.com/img/vehicles/13007738/1/v-0x8d37b6956e01daf/800.jpg"),
 					3 => new Car(3, "Dodge", "Ram 1500 Laramie", 2014, 37998, "11K", "Clackamas (OR)", 1, 15, "Black", "Red", "Automatic", "4WD", "5.7L", 8, 395, 410, "https://img2.carmax.com/img/vehicles/13007730/1/v-0x8d37a9bd9947d5a/800.jpg"),
-					4 => new Car(4, "BMW", "M3", 2015, 57998, "17K", "Beaverton (OR)", 0, 17, "Black", "White", "Manual 6 Speed", "2WD", "3.0L", 6, 425, 406, "https://img2.carmax.com/img/vehicles/12952016/1/v-0x8d351ed7afffe31/800.jpg")
+					4 => new Car(4, "BMW", "M3", 2015, 57998, "17K", "Beaverton (OR)", 0, 17, "Black", "White", "Manual 6 Speed", "2WD", "3.0L", 6, 425, 406, "https://img2.carmax.com/img/vehicles/12952016/1/v-0x8d351ed7afffe31/800.jpg"),
+					5 => new Car(5, "Dodge", "Ram 1500 Laramie", 2015, 40998, "3K", "Spokane (WA)", 9, 15, "Black", "Silver", "Automatic", "4WD", '5.7L', 8, 395, 407, "https://img2.carmax.com/img/vehicles/13007778/1/v-0x8d3708194a42c40/800.jpg"),
+					6 => new Car(6, "Porsche", "Cayenne GTS", 2010, 40998, "39K", "Beaverton (OR)", 1, 13, "Brown", "White", "Automatic", "4WD", "4.8L", 8, 405, 369, "https://img2.carmax.com/img/vehicles/12455858/1/v-0x8d2da6ace58a61b/800.jpg"),
+					7 => new Car(7, "Dodge", "Durango Limited", 2015, 35998, "19K", "Clackamas (OR)", 1, 17, "Black", "White", "Automatic", "4WD", "3.6L", 6, 290, 260, "https://img2.carmax.com/img/vehicles/12613332/1/v-0x8d37a0628cd4f29/800.jpg"),
+					8 => new Car(8, "Jeep", "Wrangler Unlimited", 2013, 30998, "46K", "Clackamas (OR)", 15, 16, "Brown", "Yellow", "Automatic", "4WD", "3.6L", 6, 285, 260, "https://img2.carmax.com/img/vehicles/12777554/1/v-0x8d342e2c75649d4/800.jpg"),
+					9 => new Car(9, "Mercedes-Benz", "C300", 2015, 33998, "15K", "Spokane (WA)", 2, 25, "Tan", "White", "Automatic", "2WD", "2.0L", 4, 241, 273, "https://img2.carmax.com/img/vehicles/12342746/1/v-0x8d2c604b701a377/800.jpg"),
+					10 => new Car(10, "BMW", "328 I", 2013, 25998, "15K", "Beaverton (OR)", 5, 23, "Black", "Silver", "Automatic", "2WD", "2.0L", 4, 240, 255, "https://img2.carmax.com/img/vehicles/12260378/1/v-0x8d2c9db8d442110/800.jpg")
 			];
 		}
 		return self::$cars;
